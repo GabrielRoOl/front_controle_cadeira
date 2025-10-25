43d693d71c30c5be87beb27faf0c8986902fa283
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-10-25 11:05:09 -0300

    feat(router, package.json): update for deploy

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
     return (
-            <BrowserRouter>
+            <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route>
                     <Route path="/" element={<Inicio />}></Route>
